f2b69efeb6e20208f4ed5c73138ee6bdb4b31f36
markwiliz30 <markwiliz30@yahoo.com>
2019-08-07 00:33:28 +0800

    with datatable

--- HRIS_v5/Source/Fasetto.Word/Controls/EmployeeTable.xaml.cs
+++ HRIS_v5/Source/Fasetto.Word/Controls/EmployeeTable.xaml.cs
@@ -23,6 +23,41 @@ namespace Fasetto.Word
         public EmployeeTable()
         {
             InitializeComponent();
+
+            Employee mark = new Employee();
+            mark._employeeId = "124asf";
+            mark._firstName = "Mark";
+            mark._middleName = "Sese";
+            mark._lastName = "Del Moro";
+            mark._nationality = "Filipino";
+            mark._eMail = "mark@gmail.com";
+            mark._contactNum = 0999999;
+            mark._religion = "INC";
+
+            Employee mark2 = new Employee();
+            mark2._employeeId = "34fgd";
+            mark2._firstName = "Mark2";
+            mark2._middleName = "Sese2";
+            mark2._lastName = "Del Moro2";
+            mark2._nationality = "Filipino2";
+            mark2._eMail = "mark@gmail.com2";
+            mark2._contactNum = 09999992;
+            mark2._religion = "INC2";
+
+            employeeTable.Items.Add(mark);
+            employeeTable.Items.Add(mark2);
+        }
+
+        public class Employee
+        {
+            public string _employeeId { get; set; }
+            public string _firstName { get; set; }
+            public string _middleName { get; set; }
+            public string _lastName { get; set; }
+            public string _nationality { get; set; }
+            public string _eMail { get; set; }
+            public int _contactNum { get; set; }
+            public string _religion { get; set; }
         }
     }
 }
